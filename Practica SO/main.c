a9cc839cc34b539736b76079246d4fafc2487ffd
valverdi <tomasvalver2002@gmail.com>
2020-06-22 18:49:52 -0300

    Respuesta 3
    asd

--- Practica SO/main.c
+++ Practica SO/main.c
@@ -5,5 +5,6 @@ int main()
 {
     printf("Hola como estas estoy probando github");
     printf("Y que te esta pareciondo github?");
+    printf("ME parece un herramienta util para poder programar");
     return 0;
 }
