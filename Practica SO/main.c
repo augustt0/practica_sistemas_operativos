a9e3aa5a72405480be4c1b099435a1b606da1d2a
ValentinNasi <valentinnasi@gmail.com>
2020-06-22 18:39:11 -0300

    Funcion Nasi creada

--- Practica SO/main.c
+++ Practica SO/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void nasi(void);
 int main()
 {
     printf("Hola como estas estoy probando github");
     return 0;
+}
+
+void nasi()
+{
+    printf("modificado en la BranchNasi")
 }
